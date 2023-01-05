7ae308ff0d3b559dc1426f02e678625bbcda5f5c
HAKDONGHAK <121990190+HAKDONGHAK@users.noreply.github.com>
2023-01-05 10:37:10 +0900

    HELLOW

---
+++ test.asm
@@ -0,0 +1,16 @@
+
+section .data
+    msg db "Hello Assembly!"
+
+section .test
+    global _start
+_start:
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, msg
+        mov rdx, 15
+
+        syscall
+        mov rax, 60
+        mov rdi, 0
+        syscall
